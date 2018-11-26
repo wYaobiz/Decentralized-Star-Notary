887d4f94404b3ebe8a98d29746b41eafa4750dec
Yao <wyao@my.ccsu.edu>
2018-11-26 16:38:24 -0500

    Add zeppelin dependacy
    Add zeppelin dependacy

--- smart_contracts/contracts/StarNotary.sol
+++ smart_contracts/contracts/StarNotary.sol
@@ -1,15 +1,14 @@
 pragma solidity ^0.4.23;
 
-import './ERC721Token.sol';
+import 'openzeppelin-solidity/contracts/token/ERC721/ERC721.sol';
 
-contract StarNotary is ERC721Token { 
+contract StarNotary is ERC721 { 
 
     struct Star { 
-        string name;
+        string name; 
     }
-    
-    mapping(uint256 => Star) public tokenIdToStarInfo;
 
+    mapping(uint256 => Star) public tokenIdToStarInfo; 
     mapping(uint256 => uint256) public starsForSale;
 
     function createStar(string _name, uint256 _tokenId) public { 
@@ -17,7 +16,7 @@ contract StarNotary is ERC721Token {
 
         tokenIdToStarInfo[_tokenId] = newStar;
 
-        ERC721Token.mint(_tokenId);
+        _mint(msg.sender, _tokenId);
     }
 
     function putStarUpForSale(uint256 _tokenId, uint256 _price) public { 
@@ -28,28 +27,18 @@ contract StarNotary is ERC721Token {
 
     function buyStar(uint256 _tokenId) public payable { 
         require(starsForSale[_tokenId] > 0);
-
+        
         uint256 starCost = starsForSale[_tokenId];
         address starOwner = this.ownerOf(_tokenId);
-
         require(msg.value >= starCost);
 
-        clearPreviousStarState(_tokenId);
-
-        transferFromHelper(starOwner, msg.sender, _tokenId);
+        _removeTokenFrom(starOwner, _tokenId);
+        _addTokenTo(msg.sender, _tokenId);
+        
+        starOwner.transfer(starCost);
 
         if(msg.value > starCost) { 
             msg.sender.transfer(msg.value - starCost);
         }
-
-        starOwner.transfer(starCost);
-    }
-
-    function clearPreviousStarState(uint256 _tokenId) private {
-        //clear approvals 
-        tokenToApproved[_tokenId] = address(0);
-
-        //clear being on sale 
-        starsForSale[_tokenId] = 0;
     }
 }
